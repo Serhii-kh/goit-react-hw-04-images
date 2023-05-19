55827ec3ed4a50767591bf7f219d032ca951f48f
Serhii Naboka <drifterzzz@gmail.com>
2023-05-19 14:53:08 +0300

    delete comments

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -57,27 +57,6 @@ export const ImageGallery = ({ searchQuery }) => {
 		}
 	};
 
-	// const handleLoadMoreBtnClick = () => {
-	// 	setLoading(true)
-	// 	setPage(page => page + 1)
-	// }
-
-	// useEffect(() => {
-	// 	if (searchQuery === '') return;
-
-	// 	try {
-	// 		fetchImages(searchQuery, page).then(({ hits }) => {
-	// 			setImages(images => [...images, ...hits]);
-	// 			setHitsSum(hitsSum => (hitsSum + hits.length));
-	// 		}
-	// 		);
-	// 	} catch (error) {
-	// 		setError(error)
-	// 	} finally {
-	// 		setLoading(false)
-	// 	}
-	// }, [page])
-
 	return (
 		<div className={css.listWrapper}>
 			{loading && <Loader />}
@@ -100,107 +79,6 @@ export const ImageGallery = ({ searchQuery }) => {
 	);
 
 }
-
-
-
-// import { Component } from 'react';
-// import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
-// import { fetchImages } from 'Api/fetchImages';
-// import { Button } from 'components/Button/Button';
-// import { Loader } from 'components/Loader/Loader';
-// import css from './ImageGallery.module.css';
-// import PropTypes from 'prop-types'
-
-// let page = 1;
-// export class ImageGallery extends Component {
-// 	state = {
-// 		images: [],
-// 		error: null,
-// 		loading: false,
-// 		totalHits: 0,
-// 		hits: 0,
-// 	};
-
-// 	componentDidUpdate(prevProps) {
-// 		const prevSearchQuery = prevProps.searchQuery;
-// 		const nextSearchQuery = this.props.searchQuery;
-
-// 		if (prevSearchQuery !== nextSearchQuery) {
-// 			page = 1;
-// 			this.setState({
-// 				loading: true,
-// 			})
-
-// 			try {
-// 				fetchImages(nextSearchQuery, page).then(({ hits, totalHits }) =>
-// 					this.setState({
-// 						images: hits,
-// 						hits: hits.length,
-// 						totalHits,
-// 					})
-// 				);
-// 				page += 1;
-// 			} catch (error) {
-// 				this.setState({ error })
-// 			}
-// 			finally {
-// 				this.setState({
-// 					loading: false,
-// 				})
-// 			}
-// 		}
-// 	}
-
-// 	handleLoadMoreBtnClick = () => {
-// 		this.setState({
-// 			loading: true,
-// 		})
-
-// 		try {
-// 			fetchImages(this.props.searchQuery, page).then(({ hits }) =>
-// 				this.setState(state => ({
-// 					images: [...state.images, ...hits],
-// 					hits: (state.hits + hits.length),
-// 				}))
-// 			);
-// 			page += 1;
-// 		} catch (error) {
-
-// 		} finally {
-// 			this.setState({
-// 				loading: false,
-// 			})
-// 		}
-// 	};
-
-// 	render() {
-// 		const { images, loading, hits, totalHits } = this.state;
-// 		const imagesLength = this.state.images.length;
-
-// 		return (
-// 			<div className={css.listWrapper}>
-// 				{loading && <Loader />}
-// 				{imagesLength > 0 && (
-// 					<>
-// 						<ul className={css.ImageGallery}>
-// 							{images.map((image) => (
-// 								<ImageGalleryItem key={image.id} image={image}
-// 								/>
-// 							))}
-// 						</ul>
-
-// 						{hits < totalHits && <Button
-// 							type="button"
-// 							onClick={this.handleLoadMoreBtnClick}>
-// 							Load more
-// 						</Button>}
-// 					</>
-// 				)}
-// 			</div>
-// 		);
-// 	}
-// }
-
 
 ImageGallery.propTypes = {
 	images: PropTypes.array,
